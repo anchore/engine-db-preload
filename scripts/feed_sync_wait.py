4c2e48e776858ff733275f2e95141c86fcd10c02
Daniel Nurmi <nurmi@anchore.com>
2018-09-28 16:03:57 -0700

    cleanup the script and make it a bit more robust/reusable
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- scripts/feed_sync_wait.py
+++ scripts/feed_sync_wait.py
@@ -6,6 +6,98 @@ import time
 import sys
 import subprocess
 
+def discover_anchore_ids():
+    # first, get the container ID of the anchore-db postgres container
+    db_id = engine_id = None
+    try:
+        cmd = "docker-compose ps -q anchore-db"
+        db_id = subprocess.check_output(cmd.split()).strip()
+
+        cmd = "docker-compose ps -q anchore-engine"
+        engine_id = subprocess.check_output(cmd.split()).strip()
+    except Exception as err:
+        raise Exception("command failed getting container ID: {}".format(cmd))
+
+    if not engine_id or not db_id:
+        raise Exception("bailing out - anchore-engine (discovered id={}) and anchore-db (discovered_id={}) must be running before executing this script".format(engine_id, db_id))
+
+    return(engine_id, db_id) 
+
+def verify_anchore_engine_available(user='admin', pw='foobar', timeout=300, health_url="http://localhost:8228/health", test_url="http://localhost:8228/v1/system/feeds"):
+    done = False
+    start_ts = time.time()
+    while not done:
+        try:
+            r = requests.get(health_url, verify=False, timeout=10)
+            if r.status_code == 200:
+                done = True
+            else:
+                print ("engine not up yet - response httpcode={} data={}".format(r.status_code, r.text))
+        except Exception as err:
+            print ("engine not up yet - exception: {}".format(err))
+        time.sleep(0.5)
+        if time.time() - start_ts >= timeout:
+            raise Exception("timed out after {} seconds".format(timeout))
+
+    done=False
+    while not done:
+        try:
+            r = requests.get(test_url, auth=(user, pw), verify=False, timeout=10)
+            if r.status_code == 200:
+                done = True
+            else:
+                print ("engine not up yet - response httpcode={} data={}".format(r.status_code, r.text))
+        except Exception as err:
+            print ("engine not up yet - exception: {}".format(err))
+        time.sleep(0.5)
+        if time.time() - start_ts >= timeout:
+            raise Exception("timed out after {} seconds".format(timeout))        
+
+    return(True)
+
+def wait_for_feed_sync(timeout=300, feeds_url="http://localhost:8228/v1/system/feeds", timer=1.0):
+    start_ts = time.time()
+    done = False
+    while not done:
+        print ("\nattempt {} / {}".format(int(time.time() - start_ts), timeout))
+        try:
+            r = requests.get(feeds_url, auth=('admin', 'foobar'), verify=False, timeout=20)
+            if r.status_code == 200:
+                data = json.loads(r.text)
+                sync_record = data[0]
+                last_sync_time = sync_record.get('last_full_sync', None)
+                if last_sync_time:
+                    print ("got last full sync time - good to go!: {}".format(last_sync_time))
+                    done=True
+                else:
+                    synced = 0
+                    total = 0
+                    synced_names = []
+                    unsynced_names = []
+                    for group in sync_record.get('groups', []):
+                        if group.get('last_sync', None):
+                            synced = synced+1
+                            synced_names.append(group.get('name', ""))
+                        else:
+                            unsynced_names.append(group.get('name', ""))
+                        total = total+1
+                    print ("not done yet {} / {} groups completed".format(synced, total))
+                    print ("\tsynced: {}\n\tunsynced: {}".format(synced_names, unsynced_names))
+            else:
+                print ("got bad response, trying again httpcode={} data={}".format(r.status_code, r.text))
+
+        except Exception as err:
+            raise Exception("cannot contact engine yet for system feeds list status - exception: {}".format(err))
+
+        time.sleep(timer)
+        if time.time() - start_ts > timeout:
+            raise Exception("timed out waiting for feeds to sync after {} seconds".format(timeout))
+
+    return(True)
+
+#### MAIN PROGRAM STARTS HERE ####
+
+# prep input from CLI
 if len(sys.argv) <= 1:
     print ("USAGE: {} <minutes to wait total> <intermediate check timer>".format(sys.argv[0]))
     sys.exit(1)
@@ -27,65 +119,49 @@ try:
 except:
     timer = 5.0
 
-url = "http://localhost:8228/v1/system/feeds"
-count = 0
-retries = (minutes*60) / timer
-done = False
-while not done and count < retries:
-    print ("\nattempt {} / {}".format(count, retries))
-    try:
-
-        r = requests.get(url, auth=('admin', 'foobar'), verify=False, timeout=15)
-        if r.status_code == 200:
-            print ("got a good 200 response")
-            data = json.loads(r.text)
-            sync_record = data[0]
-            last_sync_time = sync_record.get('last_full_sync', None)
-            if last_sync_time:
-                print ("got last full sync time - good to go!: {}".format(last_sync_time))
-                done=True
-            else:
-                synced = 0
-                total = 0
-                synced_names = []
-                unsynced_names = []
-                for group in sync_record.get('groups', []):
-                    if group.get('last_sync', None):
-                        synced = synced+1
-                        synced_names.append(group.get('name', ""))
-                    else:
-                        unsynced_names.append(group.get('name', ""))
-                    total = total+1
-                print ("not done yet {} / {} groups completed".format(synced, total))
-                print ("\tsynced: {}\n\tunsynced: {}".format(synced_names, unsynced_names))
-        else:
-            print ("got bad response, trying again  {}/{}".format(count, retries))
-
-    except Exception as err:
-        print ("exception while parsing result: {}".format(err))
-
-    count = count + 1
-    time.sleep(timer)
-
-done=1
-if done:
-    cmds = [
-        "docker-compose exec anchore-db /bin/bash -c".split() + ['pg_dump -U postgres -Z 9 > /docker-entrypoint-initdb.d/anchore-bootstrap.sql.gz'],
-        'docker-compose stop'.split(),
-        'docker commit aevolume_anchore-db_1 anchore/engine-db-preload:latest'.split(),
-        'docker-compose down --volumes'.split(),
-    ]
-    for cmd in cmds:
-        try:
-            sout = subprocess.check_output(cmd)
-            print ("CMD: {} OUTPUT: {}".format(cmd, sout))
-        except Exception as err:
-            print ("command failed: {}".format(cmd))
-            print ("bailing out")
-            sys.exit(1)
-else:
-    print ("timed out waiting for feed sync to complete")
+# before we get started, verify that anchore-engine and anchore-db are up and running
+try:
+    engine_id, db_id = discover_anchore_ids()
+except Exception as err:
+    print ("could not discover anchore-engine and anchore-db IDs - exception: {}".format(err))
     sys.exit(1)
+print ("got container IDs: engine={} db={}".format(engine_id, db_id))
 
-print ("SUCCESS!")
+# next, ensure that anchore-engine is fully up and responsive, ready to handle feed sync list API call
+try:
+    rc = verify_anchore_engine_available(timeout=300)
+except Exception as err:
+    print ("anchore-engine is not running or available - exception: {}".format(err))
+    sys.exit(1)
+print ("verified that anchore-engine is up and ready")
+
+
+# enter loop that exits if too much time has passed, or initial feed sync has completed
+try:
+    rc = wait_for_feed_sync(timeout=minutes*60, timer=timer)
+except Exception as err:
+    print ("could not verify feed sync has completed - exception: {}".format(err))
+    sys.exit(1)
+print ("verified feed sync has completed")
+
+# finally, run each of these commands in series to dump the database SQL, stop the containers, commit the DB container as a new image, and bring tear everything else down
+final_prepop_container_image = "anchore/engine-db-preload:latest"
+cmds = [
+    'docker-compose stop anchore-engine'.split(),
+    "docker-compose exec anchore-db /bin/bash -c".split() + ['pg_dump -U postgres -Z 9 > /docker-entrypoint-initdb.d/anchore-bootstrap.sql.gz'],
+    'docker-compose stop'.split(),
+    "docker commit {} {}".format(db_id, final_prepop_container_image).split(),
+    'docker-compose down --volumes'.split(),
+]
+for cmd in cmds:
+    try:
+        print ("CMD: {}".format(cmd))
+        sout = subprocess.check_output(cmd)
+        print ("\tOUTPUT: {}".format(sout))
+    except Exception as err:
+        print ("CMD failed: {}".format(cmd))
+        print ("bailing out")
+        sys.exit(1)
+
+print ("SUCCESS: new prepopulated container image created: {}".format(final_prepop_container_image))
 sys.exit(0)
